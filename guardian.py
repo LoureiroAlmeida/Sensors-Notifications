22f3b6c70cbd757088697e984a6d3449abfe5360
nataliagasol <nataliagasol@gmail.com>
2018-02-23 15:55:38 +0100

    Started coding main

---
+++ guardian.py
@@ -0,0 +1,20 @@
+
+
+class Guardian(object):
+    """HomeGuardina is a security system for python"""
+    def __init__(self):
+        super(Guardian,self).__init__()
+
+
+    def main(self):
+        # crearsensores
+        # while True:
+        #       leersensores()
+        #        if condiciones:
+        #           notificar()
+        #        sleep(1)
+        pass
+
+if __name__=='__main__':
+    guardian = Guardian()
+    guardian.main
