c0804769c0e5740bc697b83082f77d3786015473
ValentinColen <javivadefiesta@gmail.com>
2018-02-27 15:38:04 +0100

    Changes in structure

--- guardian.py
+++ guardian.py
@@ -1,18 +1,56 @@
+import time
 
+import sensors
 
 class Guardian(object):
-    """HomeGuardina is a security system for python"""
+    """HomeGuardian is a security system for python"""
     def __init__(self):
         super(Guardian,self).__init__()
+        self.binay_sensors_names = [
+            "bsensor1", "bsensor2", "bsensor3"
+        ]
+        self.numeric_sensors_names = [
+            "nsensor1", "nsensor2", "nsensor3"
+        ]
+        self.numeric_sensors = []
+        self.binay_sensors = []
+        self.sensors = {}
+        self.sensors["binary"] = {}
+        self.sensors["numeric"] = {}
 
+    def create_sensors(self):
+        for name in self.numeric_sensors_names:
+            self.numeric_sensors.append(
+                sensors.mocksensors.MockNumericSensor(name)
+            )
+        for name in self.binay_sensors_names:
+            self.binay_sensors.append(
+                sensors.mocksensors.MockBinarySensor(name)
+            )
+        pass
+
+    def get_state(self):
+        #update
+        for sensor in self.binay_sensors:
+            sensor.update_state()
+            sensors["binary"][sensor.name] = sensor.get_state()
+
+        for sensor in self.numeric_sensors:
+            sensor.update_value()
+            sensors["numeric"][sensor.name] = sensor.get_value()
+
+        return sensors
 
     def main(self):
         # crearsensores
-        # while True:
-        #       leersensores()
+        self.create_sensors()
+        while True:
+            state = self.get_state()
         #        if condiciones:
         #           notificar()
         #        sleep(1)
+            print(state)
+            time.sleep(1)
         pass
 
 if __name__=='__main__':
